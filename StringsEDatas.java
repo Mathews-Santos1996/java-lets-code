358664b74c40fc167fcf4dd085a3cd977e657509
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-05-31 15:36:29 -0300

    String

---
+++ StringsEDatas.java
@@ -0,0 +1,21 @@
+public class StringsEDatas {
+
+    public static void main (String [] args) {
+        // Olá, {nome}, Hoje é {dia-da-semana}, BOM DIA 
+
+        String nome = "Mathews";
+        System.out.println(nome.toUpperCase()); //toUpperCase -> transforma todas as letras em maiúsculas
+        System.out.println(nome.toLowerCase()); //toLowerCase -> transforma todas as letras em minúsculas
+        System.out.println(nome.length()); // tamanho da string
+
+        String nomeOutro = "mathews";
+        System.out.println(nome.equals(nomeOutro)); // equals -> faz uma comparação entre as strings, depois retorna true ou false
+        
+
+
+
+
+
+    }
+    
+}
