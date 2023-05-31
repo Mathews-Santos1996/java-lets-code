86a8e5e4478b080aed3d3c5965e31845920b18ce
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-05-31 11:42:17 -0300

    usando comandos: switch e break

--- EstruturasCondicionais.java
+++ EstruturasCondicionais.java
@@ -2,8 +2,8 @@ public class EstruturasCondicionais {
 
     public static void main (String [] args){
 
-        int nota =-50;
-
+        int nota = -50;
+        String graduacao;
         // se nota maior ou igual a 70, então aprovado
         // if-else
 
@@ -16,23 +16,33 @@ public class EstruturasCondicionais {
         // A 80 B 70 C 60 D 0
 
         if (nota >= 80) 
-            System.out.println("Graduação A");
+            {graduacao = "A";}
 
         else if (nota < 80 && nota >= 70 )
-            System.out.println("Graduação B");
+            {graduacao = "B";}
 
         else if (nota < 70 && nota >= 60 )
-            System.out.println("Graduação C");
+            {graduacao = "C";}
 
         else if (nota < 60 && nota >= 0 )
-            System.out.println("Graduação D");
+            {graduacao = "D";}
 
         else 
-            System.out.println("Nota inválida");
+            {graduacao = null;}
 
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado!");
+                break;
+            case "C":
+            case "D":
+                 System.out.println("Aluno não aprovado.");
+                break;
+            default:
+                System.out.println("Graduação inválida"); 
 
-
-
+       }
 
     }
     
