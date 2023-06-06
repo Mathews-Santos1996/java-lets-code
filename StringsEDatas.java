695408f04eacc72bbe69439638479e9f869d39fb
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-06-06 10:50:32 -0300

    hora atual/saudação

--- StringsEDatas.java
+++ StringsEDatas.java
@@ -1,21 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+ 
+
 public class StringsEDatas {
 
     public static void main (String [] args) {
         // Olá, {nome}, Hoje é {dia-da-semana}, BOM DIA 
 
-        String nome = "Mathews";
-        System.out.println(nome.toUpperCase()); //toUpperCase -> transforma todas as letras em maiúsculas
-        System.out.println(nome.toLowerCase()); //toLowerCase -> transforma todas as letras em minúsculas
-        System.out.println(nome.length()); // tamanho da string
+        String nome = "Mathews";  
+        //ISO 8601
 
-        String nomeOutro = "mathews";
-        System.out.println(nome.equals(nomeOutro)); // equals -> faz uma comparação entre as strings, depois retorna true ou false
-        
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje.getDayOfWeek()); 
+        Locale brasil = new Locale("pt","BR");
+        String diaSemana = (hoje.getDayOfWeek().getDisplayName(TextStyle.FULL,brasil));
+        String saudacao;
+         LocalDateTime agora = LocalDateTime.now();
+            if (agora.getHour()>= 0 && agora.getHour()<12) {
+                saudacao = "Bom dia";
+            } else if (agora.getHour()>= 12 && agora.getHour()<18) {
+                saudacao = "Boa tarde";
+            } else if (agora.getHour()>= 18 && agora.getHour()<24) {
+                  saudacao = "Boa noite";}
+            else {saudacao = "Olá";}
 
-
-
-
-
+        System.out.printf ("Olá, %s. Hoje é %s, %s.%n",nome, diaSemana,saudacao.toUpperCase());
+     }
     }
-    
-}
