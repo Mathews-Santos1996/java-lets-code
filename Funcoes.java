dd67372098f5ce070a83b3c641d0831e9c09911d
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-06-12 11:20:20 -0300

    saudação

---
+++ Funcoes.java
@@ -0,0 +1,12 @@
+public class Funcoes {
+
+    public static void main (String [] args){
+        String nome = "Let's Code";
+        saudacao(nome);
+    }
+    
+    public static void saudacao(String nome) {
+        System.out.println("Hello, " + nome);
+    }
+}
+
