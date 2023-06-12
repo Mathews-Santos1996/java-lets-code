8a9dcebd9bbbea8f88fbe9c8e66b7caab13d4496
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-06-12 10:08:24 -0300

    maior/menor/media

--- Arrays.java
+++ Arrays.java
@@ -2,19 +2,26 @@ public class Arrays {
 
     public static void main (String [] args){
 
-        int[] numeros = new int [5];
-        // [0] [1] [2] [3] [4]
+        int[] numeros = {9, 10, 12, 25, 2};
+        
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media = 0;
 
-        numeros[0] = 1;
-        numeros[1] = 2;
-        numeros[2] = 3;
-        numeros[3] = 4;
-        numeros[4] = 5;
+        for (int i = 0; i <numeros.length; i++){
 
-        for (int i=0; i < numeros.length; i++){
-            System.out.println(numeros[i]);
+            if (numeros[i] > maior) {
+                maior = numeros [i];
+                }
+            
+            if (numeros [i] < menor){
+                menor = numeros [i];
+                }
+            media += numeros[i]; 
+            }
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + media/numeros.length);
 
         }
-    }
-    
-}
+ }
