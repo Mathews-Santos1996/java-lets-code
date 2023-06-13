fc20bcfb176692af58f516d859e237c8c0dc2162
Mathews <Mathews-Santos1996@users.noreply.github.com>
2023-06-13 08:41:45 -0300

    função return

--- Funcoes.java
+++ Funcoes.java
@@ -1,12 +1,12 @@
 public class Funcoes {
 
     public static void main (String [] args){
-        String nome = "Let's Code";
-        saudacao(nome);
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
     }
     
-    public static void saudacao(String nome) {
-        System.out.println("Hello, " + nome);
+    public static int soma (int a, int b) {
+        return a+b;
     }
 }
 
